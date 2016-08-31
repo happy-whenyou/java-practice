c995daa200462b3026b1b971eb7a51bf4e764db1
GATech <gatech@Venus-iMac.local>
2016-08-30 20:55:46 -0700

    Assignment 1

---
+++ assignment1/src/LargestPrimeNumber.java
@@ -0,0 +1,19 @@
+public class LargestPrimeNumber {
+
+	public static void main(String[] args) {
+		LargestPrimeNumber lpn = new LargestPrimeNumber();
+		int a = lpn.prime(20);
+		System.out.println(" Largest Prime number smaller than 20 is " + a);
+	}
+	
+	/**
+	 * Used to find the largest prime number
+	 * smaller than the passed in number
+	 * @param a
+	 * @return
+	 */
+	private int prime(int a) {
+		//TODO Actual logic here
+		return 0;
+	}
+}
