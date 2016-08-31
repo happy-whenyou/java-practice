d4dbf663408609ce85c07a7aa320ebb530622023
GATech <gatech@Venus-iMac.local>
2016-08-30 21:01:10 -0700

    Adding more

---
+++ assignment1/src/ConvertDecimalToBinary.java
@@ -0,0 +1,28 @@
+//Problem 4
+
+public class ConvertDecimalToBinary {
+
+	public static void main(String[] args) {
+		ConvertDecimalToBinary convert = new ConvertDecimalToBinary();
+		String ten_binary = convert.toBinary(10);
+		System.out.println("Binary Representation of 10 is " + ten_binary);
+		String twenty_binary = convert.toBinary(20);
+		System.out.println("Binary Representation of 20 is " + twenty_binary);
+		
+	}
+	
+	/**
+	 * Everything in computers is 1 or 0 which is known as binary.
+	 * So whatever you store/write in computers is binary. 
+	 * This is a simple implementation which should convert a number to its binary representation
+	 * For Example : 2 should be converted to 10 
+	 * 3 should be converted to 11
+	 * 4 should be converted to 100
+	 * @param a
+	 * @return
+	 */
+	private String toBinary(int a) {
+		//TODO Actual Logic here;
+		return "";
+	}
+}
